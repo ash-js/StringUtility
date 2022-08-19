16404d4cd7dd3210f510a1439ceeae4b19941d82
sargo <sargo@DESKTOP-U9M6VIT>
2022-08-19 11:52:08 +1000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence =
+                "This is going to be a really really really really really really really really really really really really long text";
+            var summery = StringUtility.SummerizeText(sentence, 25);
+            Console.WriteLine(summery);
+        }
+
+        
+    }
+}
+ 
