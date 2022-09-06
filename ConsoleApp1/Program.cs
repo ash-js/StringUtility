6dffea8e496a9633c3424a09088161196329f169
A. Abdal <ashor2008@hotmail.com>
2022-09-07 09:53:05 +1000

    New fetures added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,55 @@
-﻿
-using System.Runtime.InteropServices;
+﻿using System.Security.Cryptography.X509Certificates;
 
-namespace ConsoleApp1
+namespace StringUtility
 {
+    public interface IActivity
+    {
+        void Execute();
+    }
+
+    public class VideoUploader : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Video Uploading...");
+        }
+    }
+    public class SendSms : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Sending SMS...");
+        }
+    }
+    public class CallWebService : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Calling web service...");
+        }
+    }
+
+    public class WorkflowEngine
+    {
+        public void Run(Workflow workflow)
+        {
+            foreach (var i in workflow.GetActivities())
+            {
+                i.Execute();
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
-        {
-            var sentence =
-                "This is going to be a really really really really really really really really really really really really long text";
-            var summery = StringUtility.SummerizeText(sentence, 25);
-            Console.WriteLine(summery);
+        { 
+            var workflow = new Workflow();
+            workflow.Add(new VideoUploader());
+            workflow.Add(new SendSms());
+            workflow.Add(new CallWebService());
+            var engine = new WorkflowEngine();
+            engine.Run(workflow);
+            Console.ReadLine();
         }
-
-        
-    }
+    } 
 }
- 
---
+++ ConsoleApp1/Workflow.cs
@@ -0,0 +1,32 @@
+﻿namespace StringUtility;
+
+public interface IWorkflow
+{
+    void Add(IActivity activity);
+    void Remove(IActivity activity);
+    IEnumerable<IActivity> GetActivities();
+}
+public class Workflow : IWorkflow
+{
+    private readonly List<IActivity> _activities;
+
+    public Workflow()
+    {
+        _activities = new List<IActivity>();
+    }
+
+    public void Add(IActivity activity)
+    {
+        _activities.Add(activity);
+    }
+
+    public void Remove(IActivity activity)
+    {
+        _activities.Remove(activity);
+    }
+
+    public IEnumerable<IActivity> GetActivities()
+    {
+        return _activities;
+    }
+}
